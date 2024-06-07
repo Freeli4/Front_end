cec3c7d54315814da88340fd0c3692be0317c34a
Freeli4 <144905733+Freeli4@users.noreply.github.com>
2024-06-07 11:43:55 +0300

    task3.js

---
+++ PR7/Task3/task3.js
@@ -0,0 +1,17 @@
+function main(data) {
+    return new Promise((resolve, reject) => {
+        if (typeof data !== 'number') {
+            reject('error');
+        } else if (data % 2 !== 0) {
+            setTimeout(() => {
+                resolve('odd');
+            }, 1000);
+        } else {
+            setTimeout(() => {
+                reject('even');
+            }, 2000);
+        }
+    });
+}
+
+module.exports = main;
